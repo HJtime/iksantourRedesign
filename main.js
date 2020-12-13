cb53a2b993de3a084d0612413214296a57818c8b
HJtime <72139168+HJtime@users.noreply.github.com>
2020-12-13 18:17:20 +0900

    JS submenu

--- main.js
+++ main.js
@@ -19,4 +19,23 @@ window.addEventListener('scroll', ()=>{
     }
 });
 
-// console.log(headerHegiht);
+const navMenu=document.querySelectorAll('.navbar__main li');
+const subMenu=document.querySelectorAll('.navbar__sub');
+
+navbar.addEventListener('mouseover', (event)=>{
+    const filter=event.target.dataset.num || event.target.parentNode.dataset.num;
+
+    if(filter===null){
+        return;
+    }
+
+    subMenu.forEach((sub)=>{
+        if(filter===sub.dataset.sub){
+            sub.classList.toggle('show');
+        }
+        else{
+            sub.classList.remove('show');
+        }
+    })
+});
+
